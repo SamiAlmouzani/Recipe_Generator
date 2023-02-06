cf9fb4f0c02ef369650f973312b6a8b1292b1022
lilys42 <lilyksica@gmail.com>
2023-02-06 15:39:58 -0500

    Added global context to keep track of the ingredients entered by the user, and added API call

---
+++ src/context/globalContext.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+interface IGlobalContextProps {
+  ingredientsList: string;
+  setIngredientsList: (i: string) => void;
+
+}
+
+export const GlobalContext = React.createContext<IGlobalContextProps>({
+  ingredientsList: "strawberry",
+  setIngredientsList: (i) => {console.log(i)},
+});
+
+export const GlobalContextProvider = (props: { children: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }) => {
+  const [currentIngredientsList, setCurrentIngredientsList] = useState("orange");
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        ingredientsList: currentIngredientsList,
+        setIngredientsList: setCurrentIngredientsList,
+      }}
+    >
+      {props.children}
+    </GlobalContext.Provider>
+  );
+};
