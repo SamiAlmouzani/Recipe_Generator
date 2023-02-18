5ec0fc44a0da28636444d54799ce8e5bf063791c
lilys42 <lilyksica@gmail.com>
2023-02-18 13:01:15 -0500

    Changed API from huggingface to OpenAI. Changed the code so that the API call is now running on the server instead of the client, and using an environment variable for the API key.

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -1,40 +1,26 @@
 import { type NextPage } from "next";
 import Link from "next/link";
-import { useRouter } from 'next/router'
-import { useSearchParams } from 'next/navigation'
-import { GetServerSideProps } from 'next'
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
+import {set} from "zod";
 
-import { useEffect } from "react";
-import { useGlobalContext } from "../context";
-
-type Recipe = { generated_text: string; title: string; ingredients: string; directions: string}
-const Results: NextPage = () => {
-  const { ingredientsList } = useGlobalContext();
+type Recipe = { text: string; index:number; logprobs: object; finish_reason: string}|null
+type Data = {
+    recipeList: Recipe[]
+}
+//When this page is loaded, the getServerSideProps function (further down) runs first, and returns a prop object to the Results component.
+//props is an array of Recipe objects.
+const Results: React.FC<Data>= (props) => {
   const [recipeText, setRecipeText] = useState("");
-  {
-
-    void query({ "inputs": ingredientsList }).then((response) => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      console.log(JSON.stringify(response));
-
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access
-      setRecipeText(JSON.stringify(response));
-
-
-
-    });
-
+    if(props.recipeList[0]!==undefined&&props.recipeList[0]!==null) {
+        console.log(props.recipeList[0].text)
+    }
+    useEffect(()=>{
+        if(props.recipeList[0]!==undefined&&props.recipeList[0]!==null) {
+            setRecipeText(props.recipeList[0].text)
+        }
+    }, [props.recipeList])
     return (
-
-
-
       <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-
-
-
         <div className="mx-auto text-left">
           <h1 className="text-2xl font-bold sm:text-3xl">...Here is a recipe to try:</h1>
         </div>
@@ -42,11 +28,7 @@ const Results: NextPage = () => {
         <div className="mt-6 w-full bg-white rounded-lg shadow-lg lg:w">
           <ul className="divide-y-2 divide-gray-100">
             <li className="p-3 hover:bg-red-600 hover:text-red-200">
-
-              <p className="font-bold">{recipeText.substring(recipeText.indexOf("directions"),
-                recipeText.length-3)}</p>
-              <p className="italic">{recipeText.substring(recipeText.indexOf("ingredients")+13,
-                recipeText.indexOf("directions"))}</p>
+              <pre className="italic">{recipeText}</pre>
 
             </li>
           </ul>
@@ -62,51 +44,57 @@ const Results: NextPage = () => {
 
       </div>
     );
-  }
 }
-async function query(data: object) {
-  const response = await fetch(
-    "https://api-inference.huggingface.co/models/flax-community/t5-recipe-generation",
-    {
-      headers: { Authorization: "Bearer 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" },
-      method: "POST",
-      body: JSON.stringify(data),
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function getServerSideProps (context) {
+    let recipeList:Recipe[]=[null]
+    try {
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+                'Authorization': "Bearer "+process.env.OPENAI_API_KEY
+            },
+            body: JSON.stringify({
+                'model': "text-davinci-003",
+                // eslint-disable-next-line @typescript-eslint/restrict-plus-operands,@typescript-eslint/no-unsafe-member-access
+                'prompt': "ingredients and directions for a recipe with "+context.query.ingredients,
+                'temperature': 0.1,
+                'max_tokens':500,
+                'top_p': 1,
+                'frequency_penalty': 0,
+                'presence_penalty': 0.5,
+                'stop': ["\"\"\""],
+            })
+        };
+        await fetch('https://api.openai.com/v1/completions', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+                recipeList=data.choices
+                console.log("returning")
+                console.log(recipeList)
+                console.log("first recipe")
+                console.log(recipeList[0])
+                return {
+                    recipeList
+                };
+            }).catch(err => {
+            console.log("Ran out of tokens for today! Try tomorrow!");
+        });
+        console.log("about to return")
+        console.log(recipeList)
+        return {
+            props: {recipeList}
+        };
+    }catch {
+        return {
+            props: {recipeList}
+        };
     }
-  );
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const result = await response.json();
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-member-access
-  return result;
 }
-
-function getRecipeTitle(recipeText: string):string {
-
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const parsedRecipe = JSON.parse(recipeText)
-
-  if (isRecipe(parsedRecipe)) {
-
-    return parsedRecipe.title;
-  }
-  else {
-
-    console.log("error: invalid recipe")
-  }
-  return "";
-}
-
-
-//stackoverflow.com/questions/38688822/how-to-parse-json-string-in-typescript
-
-function isRecipe(o: any): o is Recipe {
-  return "name" in o && "description" in o
-}
-
 
 export default Results;
-
-// random comment to change the file so I can recommit it
