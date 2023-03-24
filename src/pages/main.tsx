ed3d0af2e4b7492352d89f11ed519e0ba550b4bf
samialmouzani <salmouza@gmail.com>
2023-03-24 17:06:35 -0400

    loading animation from results page to recipe page

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,113 +1,113 @@
 import { type NextPage } from "next";
 import Link from "next/link";
-import {useContext, useEffect, useState} from "react";
+import { useContext, useEffect, useState } from "react";
 import { useGlobalContext } from '../context';
-import {getAuth} from "firebase/auth";
-import {FirebaseAuth} from "@firebase/auth-types";
+import { getAuth } from "firebase/auth";
+import { FirebaseAuth } from "@firebase/auth-types";
 
 //localIngredients is the local value updated every time the contents of the text box are changed
-let localIngredientsList:string
+let localIngredientsList: string
 const RecipeGenerator: NextPage = () => {
 
   //Import the global variable ingredientsList, and it's setter function, from the global context (defined in the context folder)
-  const { ingredientsList,setIngredientsList } = useGlobalContext();
-  console.log("global context "+ingredientsList);
+  const { ingredientsList, setIngredientsList } = useGlobalContext();
+  console.log("global context " + ingredientsList);
 
   //Import the current user.
-  const {currentUser, setCurrentUser}=useGlobalContext();
+  const { currentUser, setCurrentUser } = useGlobalContext();
 
   // Check if its loading
   const [isLoading, setIsLoading] = useState(false);
 
-  console.log("current user: (accessed from main screen)"+currentUser.displayName)
+  console.log("current user: (accessed from main screen)" + currentUser.displayName)
   return (
     <section className="bg-gray-50">
-  <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center relative">
-    <div className="absolute left-10 top-10">
-      <Link
-        href={{
-          pathname: '/all_saved_recipes',
-          query: {
-            uid: currentUser.uid,
-          },
-        }}
-      >
-        <button className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto">
-          See All Recipes
-        </button>
-      </Link>
-    </div>
-    <div className="mx-auto max-w-xl text-center">
-      <h1 className="text-3xl font-extrabold sm:text-5xl">
-        <strong className="font-extrabold text-red-700 sm:block">
-          Enter Ingredients
-        </strong>
-      </h1>
-      
-      {isLoading ? (
-        <div className="flex justify-center items-center mt-8">
-          <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
-        </div>
-      ) : (
-      <div className="mt-8 flex flex-wrap justify-center gap-4">
-        <IngredientsInput />
-        <Link
-          href={{
-            pathname: '/results',
-            query: {
-              ingredients: ingredientsList,
-            },
-          }}
-        >
-          <button 
-            className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
-            onClick={() => {
-              setIsLoading(true);
+      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center relative">
+        <div className="absolute left-10 top-10">
+          <Link
+            href={{
+              pathname: '/all_saved_recipes',
+              query: {
+                uid: currentUser.uid,
+              },
             }}
           >
-            Enter
-          </button>
-        </Link>
-      </div>
-      )
-    }
-      <div className="absolute right-10 top-10">
-        <Link
-          href={{
-            pathname: '/favorites',
-            query: {
-              uid: currentUser.uid,
-            },
-          }}
-        >
-          <button className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto">
-            Favorites
-          </button>
-        </Link>
-      </div>
-      <div className="absolute bottom-10 left-10">
-      <Link
-          href={{
-            pathname: '/upload_recipe',
-            query: {
-              uid: currentUser.uid,
-            },
-          }}
-        >
-          <button className="block w-full rounded bg-green-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-green-700 focus:outline-none focus:ring active:bg-green-500 sm:w-auto">
-            Upload Recipe
-          </button>
-        </Link>
-      </div>
-    </div>
-  </div>
-</section>
+            <button className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto">
+              See All Recipes
+            </button>
+          </Link>
+        </div>
+        <div className="mx-auto max-w-xl text-center">
+          <h1 className="text-3xl font-extrabold sm:text-5xl">
+            <strong className="font-extrabold text-red-700 sm:block">
+              Enter Ingredients
+            </strong>
+          </h1>
 
-    );
+          {isLoading ? (
+            <div className="flex justify-center items-center mt-8">
+              <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
+            </div>
+          ) : (
+            <div className="mt-8 flex flex-wrap justify-center gap-4">
+              <IngredientsInput />
+              <Link
+                href={{
+                  pathname: '/results',
+                  query: {
+                    ingredients: ingredientsList,
+                  },
+                }}
+              >
+                <button
+                  className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+                  onClick={() => {
+                    setIsLoading(true);
+                  }}
+                >
+                  Enter
+                </button>
+              </Link>
+            </div>
+          )
+          }
+          <div className="absolute right-10 top-10">
+            <Link
+              href={{
+                pathname: '/favorites',
+                query: {
+                  uid: currentUser.uid,
+                },
+              }}
+            >
+              <button className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto">
+                Favorites
+              </button>
+            </Link>
+          </div>
+          <div className="absolute bottom-10 left-10">
+            <Link
+              href={{
+                pathname: '/upload_recipe',
+                query: {
+                  uid: currentUser.uid,
+                },
+              }}
+            >
+              <button className="block w-full rounded bg-green-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-green-700 focus:outline-none focus:ring active:bg-green-500 sm:w-auto">
+                Upload Recipe
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  );
 }
 //Ingredients input field
-function IngredientsInput(){
-    const { ingredientsList,setIngredientsList } = useGlobalContext();
+function IngredientsInput() {
+  const { ingredientsList, setIngredientsList } = useGlobalContext();
   //Save the state of the value entered into the text box
   const [ingredients, setIngredients] = useState("");
   return (
@@ -116,7 +116,7 @@ function IngredientsInput(){
         type="text"
         value={ingredients}
         //whenever the value of the text box is changed, save it in localIngredientsList so it can be accessed in the outer function above
-        onChange={e => {setIngredients(e.target.value);localIngredientsList=e.target.value;console.log(localIngredientsList);setIngredientsList(localIngredientsList)}}
+        onChange={e => { setIngredients(e.target.value); localIngredientsList = e.target.value; console.log(localIngredientsList); setIngredientsList(localIngredientsList) }}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="avocado"
         required
--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -1,22 +1,24 @@
 import Link from "next/link";
-import React, {useEffect, useState} from 'react';
-import {set} from "zod";
-import {child, get, getDatabase, ref} from "firebase/database";
-import {db, auth, app} from "../context/firebaseSetup";
-import {getAnalytics} from "firebase/analytics";
-import {getAuth} from "firebase/auth";
-import {useGlobalContext} from "../context";
-type RecipeFromAPI = { text: string; index:number; logprobs: object; finish_reason: string}|null
+import React, { useEffect, useState } from 'react';
+import { set } from "zod";
+import { child, get, getDatabase, ref } from "firebase/database";
+import { db, auth, app } from "../context/firebaseSetup";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { useGlobalContext } from "../context";
+type RecipeFromAPI = { text: string; index: number; logprobs: object; finish_reason: string } | null
 
 //----For definitions for the Recipe, RecipeFromAPI, RecipeContext, and Comment types, see index.d.ts in the types folder----
 
 //When this page is loaded, the getServerSideProps function (further down) runs first, and returns a prop object to the Results component.
 //props is an array of Recipe objects.
-const Results: React.FC<RecipeArray>= (props) => {
+const Results: React.FC<RecipeArray> = (props) => {
+
+    const [isLoading, setIsLoading] = useState(false);
 
     //Import the current user.
-    const {currentUser, setCurrentUser}=useGlobalContext();
-    console.log("current user: (accessed from main screen)"+currentUser.displayName)
+    const { currentUser, setCurrentUser } = useGlobalContext();
+    console.log("current user: (accessed from main screen)" + currentUser.displayName)
     console.log("recipes from props\n")
     console.log(props.recipeList)
     // @ts-ignore
@@ -27,48 +29,54 @@ const Results: React.FC<RecipeArray>= (props) => {
             <div className="mx-auto text-left">
                 <h1 className="text-2xl font-bold sm:text-3xl">...Here are some recipes to try:</h1>
             </div>
-
-            <div className="mt-6 w-full bg-white rounded-lg shadow-lg lg:w">
-                <div>
-                    {props.recipeList.map((recipe:Recipe) =>
-                        <div>
-                            <div className="mt-6 w-full bg-white rounded-lg shadow-lg lg:w">
-                                <Link href={{
-                                    pathname: '/recipe',
-                                    query: {
-                                        id:recipe.id,
-                                        title:recipe.title,
-                                        text:recipe.text,
-                                        image:recipe.image,
-                                        ingredients:recipe.ingredients,
-                                        averageRating:recipe.averageRating,
-                                        uploadedBy:recipe.uploadedBy,
-                                        //@ts-ignore
-                                        comments:recipe.comments,
-                                        //@ts-ignore
-                                        ratingMap:recipe.ratingMap,
-                                        ratingSum:recipe.ratingSum,
-                                        totalRatings:recipe.totalRatings
-                                    }
-                                }} as={`recipe/$recipeText}`}>
-                                    <ul className="divide-y-2 divide-gray-100">
-                                        <li className="p-3 hover:bg-red-600 hover:text-red-200">
-                                            <pre className="italic">{recipe.title}</pre>
-                                        </li>
-                                    </ul>
-                                </Link>
-                            </div>
-                        </div>)}
+            {isLoading ? (
+                <div className="flex justify-center items-center mt-8">
+                    <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
                 </div>
+            ) : (
+                <div className="mt-6 w-full bg-white rounded-lg shadow-lg lg:w">
+                    <div>
+                        {props.recipeList.map((recipe: Recipe) =>
+                            <div>
+                                <div className="mt-6 w-full bg-white rounded-lg shadow-lg lg:w">
+                                    <Link href={{
+                                        pathname: '/recipe',
+                                        query: {
+                                            id: recipe.id,
+                                            title: recipe.title,
+                                            text: recipe.text,
+                                            image: recipe.image,
+                                            ingredients: recipe.ingredients,
+                                            averageRating: recipe.averageRating,
+                                            uploadedBy: recipe.uploadedBy,
+                                            //@ts-ignore
+                                            comments: recipe.comments,
+                                            //@ts-ignore
+                                            ratingMap: recipe.ratingMap,
+                                            ratingSum: recipe.ratingSum,
+                                            totalRatings: recipe.totalRatings
+                                        }
+                                    }} as={`recipe/$recipeText}`} onClick={() => setIsLoading(true)}>
+                                        <ul className="divide-y-2 divide-gray-100">
+                                            <li className="p-3 hover:bg-red-600 hover:text-red-200">
+                                                <pre className="italic">{recipe.title}</pre>
+                                            </li>
+                                        </ul>
+                                    </Link>
+                                </div>
+                            </div>)}
+                    </div>
 
-                <Link href="/main">
-                    <button
-                        className="block w-full mt-6 rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
-                    >
-                        Back
-                    </button>
-                </Link>
-            </div></div>
+                    <Link href="/main">
+                        <button
+                            className="block w-full mt-6 rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+                        >
+                            Back
+                        </button>
+                    </Link>
+                </div>
+            )}
+        </div>
     );
 }
 /*
@@ -79,86 +87,86 @@ query is an object that has an ingredients field, which is just the text the use
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 // eslint-disable-next-line @typescript-eslint/require-await
-export async function getServerSideProps (context) {
-    let titleList:string[]=[]
-    let titleListNoDuplicates:string[]=[]
-    let recipeList:Recipe[]=[]
-    let tempRatingMap:Map<string,number>=new Map<string, number>()
+export async function getServerSideProps(context) {
+    let titleList: string[] = []
+    let titleListNoDuplicates: string[] = []
+    let recipeList: Recipe[] = []
+    let tempRatingMap: Map<string, number> = new Map<string, number>()
     //This try/catch block uses the API to generate only a recipe title. Comment out this block to pull recipes from the database instead for testing
     try {
-                const requestOptions = {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
-                        'Authorization': "Bearer "+process.env.OPENAI_API_KEY
-                    },
-                    body: JSON.stringify({
-                        'model': "text-davinci-003",
-                        // eslint-disable-next-line @typescript-eslint/restrict-plus-operands,@typescript-eslint/no-unsafe-member-access
-                        'prompt': "name of a recipe that contains "+context.query.ingredients,
-                        'temperature': 0.7,
-                        //max_tokens is the max number of words that can be returned for one recipe. This is set to 20 just because I didn't need all
-                        //the directions for testing, but for demoing we'll need to set it higher (it cuts off the directions)
-                        'max_tokens':20,
-                        'top_p': 1,
-                        //To generate additional recipes, change n
-                        'n':7,
-                        'frequency_penalty': 0,
-                        'presence_penalty': 0.5,
-                        'stop': ["\"\"\""],
-                    })
-                };
-                await fetch('https://api.openai.com/v1/completions', requestOptions)
-                    .then(response => response.json())
-                    .then(data => {
-                        //Store all of the generated recipe titles in the titleList array
-                        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
-                        titleList=data.choices.map((r:RecipeFromAPI)=>{
-                            //Sometimes the API will return text before the title. Remove anything above the last occurrence of a newline
-                            // @ts-ignore
-                            return r.text.substring(r.text.lastIndexOf('\n')+1,r.text.length)
-                        })
-                        //Remove duplicates from titleList
-                        titleList.forEach((r)=>{
-                            //Check whether this recipe was already added to titleListNoDuplicates
-                            if(!(titleListNoDuplicates.indexOf(r)>-1)) {
-                                titleListNoDuplicates.push(r)
-                            }
-                        })
-                        console.log("Titles after removing duplicates")
-                        console.log(titleListNoDuplicates)
-                        //Map each of these titles into a recipe array (all fields of each recipe will be empty, except for the title
-                        recipeList=titleListNoDuplicates.map((title)=>{
-                            let recipe:Recipe={
-                                id:"",
-                                title:title,
-                                text:"",
-                                image:"",
-                                ingredients:context.query.ingredients,
-                                averageRating:0,
-                                uploadedBy:"0",
-                                //@ts-ignore
-                                comments:"",
-                                ratingMap: JSON.stringify(Array.from(tempRatingMap.entries())),
-                                ratingSum:0,
-                                numberofRatings:0
-                            }
-                            return recipe
-                        })
-                    }).catch(err => {
-                    console.log(err);
-                });
-                return {
-                    props: {recipeList}
-                };
-            }catch {
-                return {
-                    props: {recipeList}
-                };
-            }
-    return{
-        props:{recipeList}
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+                'Authorization': "Bearer " + process.env.OPENAI_API_KEY
+            },
+            body: JSON.stringify({
+                'model': "text-davinci-003",
+                // eslint-disable-next-line @typescript-eslint/restrict-plus-operands,@typescript-eslint/no-unsafe-member-access
+                'prompt': "name of a recipe that contains " + context.query.ingredients,
+                'temperature': 0.7,
+                //max_tokens is the max number of words that can be returned for one recipe. This is set to 20 just because I didn't need all
+                //the directions for testing, but for demoing we'll need to set it higher (it cuts off the directions)
+                'max_tokens': 20,
+                'top_p': 1,
+                //To generate additional recipes, change n
+                'n': 7,
+                'frequency_penalty': 0,
+                'presence_penalty': 0.5,
+                'stop': ["\"\"\""],
+            })
+        };
+        await fetch('https://api.openai.com/v1/completions', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                //Store all of the generated recipe titles in the titleList array
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+                titleList = data.choices.map((r: RecipeFromAPI) => {
+                    //Sometimes the API will return text before the title. Remove anything above the last occurrence of a newline
+                    // @ts-ignore
+                    return r.text.substring(r.text.lastIndexOf('\n') + 1, r.text.length)
+                })
+                //Remove duplicates from titleList
+                titleList.forEach((r) => {
+                    //Check whether this recipe was already added to titleListNoDuplicates
+                    if (!(titleListNoDuplicates.indexOf(r) > -1)) {
+                        titleListNoDuplicates.push(r)
+                    }
+                })
+                console.log("Titles after removing duplicates")
+                console.log(titleListNoDuplicates)
+                //Map each of these titles into a recipe array (all fields of each recipe will be empty, except for the title
+                recipeList = titleListNoDuplicates.map((title) => {
+                    let recipe: Recipe = {
+                        id: "",
+                        title: title,
+                        text: "",
+                        image: "",
+                        ingredients: context.query.ingredients,
+                        averageRating: 0,
+                        uploadedBy: "0",
+                        //@ts-ignore
+                        comments: "",
+                        ratingMap: JSON.stringify(Array.from(tempRatingMap.entries())),
+                        ratingSum: 0,
+                        numberofRatings: 0
+                    }
+                    return recipe
+                })
+            }).catch(err => {
+                console.log(err);
+            });
+        return {
+            props: { recipeList }
+        };
+    } catch {
+        return {
+            props: { recipeList }
+        };
+    }
+    return {
+        props: { recipeList }
     }
 }
 export default Results;
