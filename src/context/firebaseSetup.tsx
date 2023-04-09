c845402bdc79a5eed3460c9820d9615938629f4b
lilys42 <lilyksica@gmail.com>
2023-04-08 21:53:09 -0400

    Added firebaseSetup.tsx back to Git so the deployment would work (it also looks like this might be fine)

---
+++ src/context/firebaseSetup.tsx
@@ -0,0 +1,45 @@
+// Import the functions you need from the SDKs you need
+import firebase, { getApps } from "firebase/app";
+import "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+import { getAuth } from 'firebase/auth';
+import {Database, DatabaseReference, getDatabase} from "firebase/database";
+import { FirebaseDatabase } from "@firebase/database-types";
+
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDJQ911w4jyDOPfYQzIQXmm9Nm3RTsHLq0",
+    authDomain: "superchef-df411.firebaseapp.com",
+    projectId: "superchef-df411",
+    storageBucket: "superchef-df411.appspot.com",
+    messagingSenderId: "920620604946",
+    appId: "superchef-df411",
+    measurementId: "G-7W09BPCPY6",
+    databaseURL: "https://superchef-df411-default-rtdb.firebaseio.com/"
+};
+
+//firebase.initializeApp(firebaseConfig);
+
+let app: firebase.FirebaseApp
+let db : Database;
+let analytics
+let auth
+// if database does not exist, do stuff
+if (getApps().length == 0) {
+    app = initializeApp(firebaseConfig);
+    db = getDatabase(app);
+    analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
+    auth = getAuth(app);
+}
+// otherwise just return the reference to the database
+export {db as db, auth as auth, app as app}
+
+//export default firebase;
